af644c3651a1cf12b5f9f589c63c9ae53ea7f0ee
Ignacio Chiaradia <ichiaradia@mobydigital.com>
2021-11-11 00:16:09 -0300

    Actualizacion login y guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './screens/home/home.component';
+import { ProductComponent } from './screens/product/product.component';
+import { ProductsComponent } from './screens/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'products', component: ProductsComponent, canActivate:[AuthGuard]},  // agregar guards
+  { path: 'login', component: LoginComponent}
+
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,7 +1,8 @@
-import { AuthService } from './../../../services/auth.service';
+import { AuthService } from '../../../auth/auth.service';
 import { User } from '../../../models/user';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ export class LoginComponent implements OnInit {
   loginForm:FormGroup;
 
 
-  constructor(private fb: FormBuilder, private _authService: AuthService) {}
+  constructor(private fb: FormBuilder, private _authService: AuthService, private router:Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
@@ -30,7 +31,11 @@ export class LoginComponent implements OnInit {
     userCredentials.email = this.loginForm.get('email')?.value;
     userCredentials.password = this.loginForm.get('password')?.value;
 
-    this._authService.login(userCredentials).subscribe(algo => {
+    this._authService.login(userCredentials).subscribe(response => {
+      
+      if(response.token){
+        this.router.navigate(['products']);
+      }
       
     });
   }
